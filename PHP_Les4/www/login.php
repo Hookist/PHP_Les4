2adabd84a12721f2fee1edf0cb9510a5b4d2ee1b
Hookist <spartanstas@gmail.com>
2016-05-16 00:38:12 +0300

    Add files via upload

---
+++ PHP_Les4/www/login.php
@@ -0,0 +1,37 @@
+  <html>
+   <title>
+
+   </title>
+   <head>
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css"> 
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script> 
+       
+
+       
+       
+   </head>
+    <body>
+        <div class="input-group">
+  <span class="input-group-addon" id="basic-addon1">UserName:</span>
+  <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+</div>
+
+<div class="input-group">
+  <input type="text" class="form-control" placeholder="Login" aria-describedby="basic-addon2">
+  <span class="input-group-addon" id="basic-addon2">@gmail.com</span>
+</div>
+
+<div class="input-group">
+  <span class="input-group-addon">Password :</span>
+  <input type="text" class="form-control" placeholder="********" aria-label="Amount (to the nearest dollar)">
+  <span class="input-group-addon">need more then 6 symbols</span>
+</div>
+        
+        <div class="input-group">
+  <span class="input-group-addon">Password Again :</span>
+  <input type="text" class="form-control" placeholder="********" aria-label="Amount (to the nearest dollar)">
+
+</div>
+      </body>
+</html>
